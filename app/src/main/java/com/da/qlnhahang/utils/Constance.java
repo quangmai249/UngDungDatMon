a58fdbaf91f0ec45a484f9ee117f6f214140c330
quangmai249 <quangmai249@gmail.com>
2022-08-24 05:08:20 +0700

    back up

--- app/src/main/java/com/da/qlnhahang/utils/Constance.java
+++ app/src/main/java/com/da/qlnhahang/utils/Constance.java
@@ -4,5 +4,5 @@ public interface Constance {
     String USERNAME = "username";
     String PASSWORD = "password";
     String PREFERENCES_NAME = "App";
-    String BASE_URL = "http://192.168.1.15/qlnhahang/";
+    String BASE_URL = "https://doanqlnhahang.000webhostapp.com/";
 }
